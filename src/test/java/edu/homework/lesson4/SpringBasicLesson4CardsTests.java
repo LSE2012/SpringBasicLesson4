fce8b10dde005539429d6808bb7d7d9c8678ea49
iuad0qak <ios.developers@aval.ua>
2023-03-23 18:21:54 +0200

    Create CardService created test methods

--- src/test/java/edu/homework/lesson4/SpringBasicLesson4CardsTests.java
+++ src/test/java/edu/homework/lesson4/SpringBasicLesson4CardsTests.java
@@ -1,9 +1,13 @@
 package edu.homework.lesson4;
 
+import edu.homework.lesson4.entity.Cards;
 import edu.homework.lesson4.repository.CardsRepository;
+import edu.homework.lesson4.services.CardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class SpringBasicLesson4CardsTests {
@@ -18,5 +22,11 @@ public class SpringBasicLesson4CardsTests {
         cards.forEach(System.out::println);
     }
 
-
+    @Test
+    public void testCardsServiceFindOneById() {
+        var cardId = 2;
+        List<Cards> cards = new CardService(cardsRepository).findAllCards();
+        System.out.println("====== testCardsServiceFindOneById() ========");
+        cards.forEach(System.out::println);
+    }
 }
